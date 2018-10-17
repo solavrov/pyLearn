ada299ea09676c2799908d7b4a228434095e3842
solavrov <2slavrov@gmail.com>
2018-10-17 11:34:36 +0300

    kivy fixing

--- kivy_test.py
+++ kivy_test.py
@@ -1,38 +1,23 @@
-from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
 
 
 class MyWidget(Widget):
 
-    def on_touch_down(self, touch):
-        color = (random(), 1, 1)
-        with self.canvas:
-            Color(*color, mode='hsv')
-            d = 30
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
-
-    def on_touch_move(self, touch):
-        touch.ud['line'].points += [touch.x, touch.y]
+    def draw_circle(self):
+        cl = Color(1, 0, 0, 1)
+        cr = Ellipse(pos=(100, 100), size=(100, 100))
+        self.canvas.add(cl)
+        self.canvas.add(cr)
 
 
 class MyApp(App):
 
     def build(self):
-        parent = Widget()
-        self.painter = MyWidget()
-        btn = Button(text="Clear")
-        btn.bind(on_release=self.clear_canvas)
-        parent.add_widget(self.painter)
-        parent.add_widget(btn)
-        return parent
-
-    def clear_canvas(self):
-        self.painter.canvas.clear()
+        w = MyWidget()
+        w.draw_circle()
+        return w
 
 
-if __name__ == "__main__":
-    MyApp().run()
+MyApp().run()
