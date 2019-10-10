0be7c7c1e40ee5f1ca3f1d1365d9ce3f933eb57c
solavrov <2slavrov@gmail.com>
2019-10-10 13:17:00 +0300

    numpy_test_2

--- nympy_test.py
+++ nympy_test.py
@@ -1,9 +1,32 @@
-import numpy as np
+from numpy import array as array, dot as dot, transpose as t
+from numpy import linalg as alg
 
-a = np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]])
-b = np.array([[1], [1], [1]])
-c = np.dot(a, b)
+a = array([[1, 2, 3],
+           [2, 5, 3],
+           [3, 3, 9]])
 
-print(c)
+b = array([[1],
+           [1],
+           [1]])
+
+c = dot(a, b)
+
+d = array([[1, 2],
+           [-3, 4]])
+
+# print(c)
+
+print(alg.det(a))
+eigen_all = alg.eig(a)
+print(eigen_all)
+eigen_vectors = eigen_all[1]
+print(eigen_vectors)
+v0 = array([eigen_vectors[:, 0]])
+v1 = array([eigen_vectors[:, 1]])
+v2 = array([eigen_vectors[:, 2]])
+print(v0, v1, v2)
+print('v0.v1=', dot(v0, t(v1)))
+print('v1.v2=', dot(v1, t(v2)))
+print('v0.v2=', dot(v0, t(v2)))
 
 
