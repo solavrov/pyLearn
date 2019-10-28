c02593b286742fcef3cb65fd2da666a2b35aa1d2
solavrov <2slavrov@gmail.com>
2019-10-28 18:32:13 +0300

    some eigen vector calcs

--- numpy_test.py
+++ numpy_test.py
@@ -17,10 +17,16 @@ d = array([[1, 2],
 # print(c)
 
 print(alg.det(a))
+
 eigen_all = alg.eig(a)
 print(eigen_all)
 eigen_vectors = eigen_all[1]
-print(eigen_vectors)
+print('\neigen vectors:\n', eigen_vectors)
+print('\ninv eigen vectors:\n', alg.inv(eigen_vectors))
+
+trans_a = dot(t(eigen_vectors), dot(a, eigen_vectors))
+print('\ntransformed a:\n', trans_a, '\n')
+
 v0 = array([eigen_vectors[:, 0]])
 v1 = array([eigen_vectors[:, 1]])
 v2 = array([eigen_vectors[:, 2]])
@@ -28,5 +34,3 @@ print(v0, v1, v2)
 print('v0.v1=', dot(v0, t(v1)))
 print('v1.v2=', dot(v1, t(v2)))
 print('v0.v2=', dot(v0, t(v2)))
-
-
