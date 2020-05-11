be62c3fdfec97f77676acca803b24e157881c044
solavrov <2slavrov@gmail.com>
2020-05-11 14:59:12 +0300

    permutations

---
+++ permutations.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+
+w = 'ФЗФТШМФТИ'
+p = permutations(w, len(w))
+p = list(set(p))
+
+for i in range(0, len(p)):
+    p[i] = ''.join(p[i])
+
+c = 0
+for i in range(0, len(p)):
+    if not('ТШ' in p[i]) and not('ФЗ' in p[i]) and not('ФТ' in p[i]):
+        c += 1
+
+print('answer:', c)
+
+
+
+
